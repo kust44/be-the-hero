279b09f310740a7081a2c65eac3f9721ec2f69e3
Gustavo Rodrigues <kust44@github.com>
2020-03-30 17:04:27 -0300

    Funcionalidades avançadas (testes unitários e de integração)

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const routes = require('./routes');
 const cors = require('cors');
 const express = require('express');
+const { errors } = require('celebrate');
 
 const app = express();
 
@@ -34,5 +35,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
